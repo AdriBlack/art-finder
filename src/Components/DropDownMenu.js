839ca7cb16a64edb7d5055d1f17773e1cba857c0
Adriana Black <adrianablancov@gmail.com>
2020-10-21 00:21:55 +0100

    parsed XML and set up currency type

--- src/Components/DropDownMenu.js
+++ src/Components/DropDownMenu.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import xmlParser from 'xml2js'
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
@@ -7,11 +9,40 @@ import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
 
 function DropDownMenu(props) {
-    const [currency, setCurrency] = useState('currency')
+    const [currency, setCurrency] = useState('')
+    const [input, setInput] = useState('currency')
+    const [list, setList] = useState('')
+
+
+    const proxyurl = "https://cors-anywhere.herokuapp.com/"
+    const url = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
+    const parser = new xmlParser.Parser()
+
+    useEffect(() => {
+        axios.get(proxyurl + url, {
+            "Content-Type": "application/json; charset=utf-8"
+        })
+            .then(response => {
+                parser.parseStringPromise(response.data)
+                    .then(result => {
+                        setList(result['gesmes:Envelope'].Cube[0].Cube[0].Cube)
+                    })
+            })
+        return () => {
+            // need to clean up
+        }
+    }, [])
 
     const handleChange = (event) => {
         setCurrency(event.target.value)
     }
+
+    const handleInputChange = (event) => {
+        console.log(event.target.value)
+        setInput(event.target.value)
+    }
+
+    console.log(list)
     return (
         <FormControl>
             <InputLabel>1 USD = 0.8110 EUR</InputLabel>
@@ -19,12 +50,14 @@ function DropDownMenu(props) {
                 value={currency}
                 onChange={handleChange}
             >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+                {list && list.map((currency) => {
+                    return (
+                        <MenuItem value={10}>{currency['$'].currency}</MenuItem>
+                    )
 
+                })}
             </Select>
-            <TextField/>
+            <TextField value={input} onChange={handleInputChange} />
         </FormControl>
     )
 
